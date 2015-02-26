3ca4ce7413c4a7706f89cb3afbd63f94eab0477f
Richard Leopold <rjleopold@gmail.com>
2015-02-25 22:27:00 -0600

    wtf 404 on delete?

---
+++ src/AureliaSchoolVS2015/wwwroot/dist/studentService.js
@@ -0,0 +1,31 @@
+﻿import {HttpClient} from 'aurelia-http-client';
+
+var url = window.location.origin + '/api/students';
+
+export class StudentService {
+    static inject() {return [HttpClient];}
+
+    constructor(http) {
+        this.http = http.request.withHeader('Content-Type', 'application/json');
+    }
+
+    getStudents() {
+        return this.http.get(url);
+    }
+
+    getStudent(id) {
+
+    }
+
+    insertStudent(data) {
+        return this.http.post(url,data);
+    }
+
+    updateStudent(data) {
+
+    }
+
+    deleteStudent(id) {
+        return this.http.delete(url + '/' + id);
+    }
+}
--- src/AureliaSchoolVS2015/wwwroot/dist/students.js
+++ src/AureliaSchoolVS2015/wwwroot/dist/students.js
@@ -1,1 +1,40 @@
-﻿
+﻿import {StudentService} from './studentService';
+
+var url = window.location.origin + '/api/students';
+
+export class Students {
+    static inject() {return [StudentService];}
+    
+    constructor(api) {
+        this.students = [];
+        this.api = api;
+        this.newStudent = {};
+        this.showAdd = false;
+    }
+
+    activate() {
+        return this.api.getStudents().then(response => {
+            this.students = response.content;
+        });
+    }
+
+    clickAdd() {
+        this.showAdd = !this.showAdd;
+    }
+
+    addStudent(data) {
+        this.api.insertStudent(data).then(response => {
+            this.students.push(response.content);
+            this.showAdd = false;
+            this.newStudent = {};
+        });
+    }
+
+    deleteStudent(idx) {
+        console.log(idx);
+        var s = this.students[idx];
+        this.api.deleteStudent(s.Id).then(response => {
+            this.students.splice(idx,1);
+        })
+    }
+}
